d8ef471681bea7ba9edb0181a8167dfde929fce2
bebaj <be.bajnoczi@gmail.com>
2024-08-10 16:50:55 +0200

    datas

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,18 @@
-INSERT INTO USERS VALUES (11, 'test', 'test');
-INSERT INTO USERS VALUES (12, 'tes', 'tes');
-INSERT INTO USERS VALUES (13, 'te', 'te');
+INSERT INTO users (username, password)
+VALUES
+    ('ash_ketchum', 'pikachu123'),
+    ('misty_waterflower', 'starmie456'),
+    ('brock_pewter', 'onix789');
+INSERT INTO pokemons (name, weight, height, abilities)
+VALUES
+    ('Pikachu', 6.0, 0.4, 'Static, Lightning Rod'),
+    ('Bulbasaur', 6.9, 0.7, 'Overgrow, Chlorophyll'),
+    ('Charmander', 8.5, 0.6, 'Blaze, Solar Power'),
+    ('Squirtle', 9.0, 0.5, 'Torrent, Rain Dish');
+
+INSERT INTO user_pokemons (user_id, pokemon_id)
+VALUES
+    (1, 1), -- Ash has Pikachu
+    (1, 2), -- Ash also has Bulbasaur
+    (2, 4), -- Misty has Squirtle
+    (3, 3); -- Brock has Charmander
